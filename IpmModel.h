80642c1446eb7c9d5f871c47f707a59d30014e1c
filikat <78378227+filikat@users.noreply.github.com>
2025-01-13 16:40:09 +0000

    Removed alternative scaling

--- IpmModel.h
+++ IpmModel.h
@@ -48,11 +48,6 @@ class IpmModel {
   int cexp_{};
   int bexp_{};
 
-  std::vector<double> colscale_{};
-  std::vector<double> rowscale_{};
-  double cscale_{};
-  double bscale_{};
-
   double norm_rhs_ = -1.0;
   double norm_obj_ = -1.0;
 
@@ -75,9 +70,7 @@ class IpmModel {
 
   // (Un)scale the matrix
   void scale();
-  void scale2();
   void unscale(Iterate& it);
-  void unscale2(Iterate& it);
 
   double normRhs();
   double normObj();
