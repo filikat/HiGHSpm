c1514cc310c781b81a710cc74a5981c049a42177
filikat <78378227+filikat@users.noreply.github.com>
2025-04-17 14:44:13 +0100

    Updated cmake to use framework accelerate on mac

--- cmake/find_stuff.cmake
+++ cmake/find_stuff.cmake
@@ -1,16 +1,18 @@
 # find stuff (may require to define CMAKE_PREFIX_PATH and/or CMAKE_INCLUDE_PATH)
 
 # BLAS
-find_library(BLAS_LIB NAMES blas)
-if(BLAS_LIB)
-    message("Found BLAS library at ${BLAS_LIB}")
-else(BLAS_LIB)
-    find_library(OPENBLAS_LIB NAMES openblas)
-    if(NOT OPENBLAS_LIB)
-        message(FATAL_ERROR "No BLAS library found")
-    endif(NOT OPENBLAS_LIB)
-    message("Found OpenBLAS library at ${OPENBLAS_LIB}")
-endif(BLAS_LIB)
+if(NOT APPLE)
+    find_library(BLAS_LIB NAMES blas)
+    if(BLAS_LIB)
+        message("Found BLAS library at ${BLAS_LIB}")
+    else(BLAS_LIB)
+        find_library(OPENBLAS_LIB NAMES openblas)
+        if(NOT OPENBLAS_LIB)
+            message(FATAL_ERROR "No BLAS library found")
+        endif(NOT OPENBLAS_LIB)
+        message("Found OpenBLAS library at ${OPENBLAS_LIB}")
+    endif(BLAS_LIB)
+endif(NOT APPLE)
 
 # Metis
 find_path(METIS_PATH NAMES "metis.h" REQUIRED)
